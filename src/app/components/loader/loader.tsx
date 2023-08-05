f63db0b12d4c12c5d1bc86cd7bdeed3fba27bb28
slvally <fieshidiq@gmail.com>
2023-08-05 13:55:15 +0700

    loader and clean code page

---
+++ src/app/components/loader/loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loaderStyle.css';
+
+const Loader = () => {
+  return (
+    <>
+    {/* Style 1 without CSS */}
+    <div className="loaderStyle">
+    </div>
+    {/*  Style 2 with CSS */}
+    {/* <div className="loaderStyle2"></div> */}
+    </>
+  );
+};
+
+export default Loader;
