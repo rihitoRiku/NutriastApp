7c2f11169a4716c96dfb9aad0a7cbe488d3efa96
Fadhil <fadhilanugrah21@gmail.com>
2023-08-07 21:55:54 +0700

    Cookies

--- src/app/actions/LoginCookies.ts
+++ src/app/actions/LoginCookies.ts
@@ -1,10 +1,18 @@
-'use server'
- 
-import { cookies } from 'next/headers'
- 
+'use server';
+
+import { cookies } from 'next/headers';
+
 const create = async (data: string) => {
-  const oneDay = 24 * 60 * 60 * 1000
-  cookies().set('Login Token', data, { expires: Date.now() - oneDay })
-}
+  // Set the cookie to expire in one day from the current time
+  const oneDay = 24 * 60 * 60 * 1000;
+  const expirationDate = Date.now() + oneDay;
+
+  // Set the "Login Token" cookie in the response headers
+  cookies().set('Login Token', data, { expires: expirationDate });
+
+  // Note: The following console.log won't display the cookie in the response,
+  // since cookies are part of the response headers and not accessible here.
+  console.log(cookies().get('Login Token'));
+};
 
 export default create;
